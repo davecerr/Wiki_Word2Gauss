530bfc489f47c29667f16edf952cefe5b97ac1c2
davide <davide@pop-os.localdomain>
2020-12-26 16:04:42 +0000

    print batches to investigate batch_size effect

--- word2gauss/words.py
+++ word2gauss/words.py
@@ -96,7 +96,7 @@ def iter_pairs(fin, vocab, batch_size=10, nsamples=2, window=5):
             vocab.tokenize_ids(doc, remove_oov=False)
             for doc in batch
         ]
-        print(text)
+        print("\n {}".format(text))
         pairs = text_to_pairs(text, vocab.random_ids,
             nsamples_per_word=nsamples,
             half_window_size=window)
