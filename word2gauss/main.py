470da410ae683fcbe541570c89244ecdb561d163
davide <davide@pop-os.localdomain>
2020-12-26 12:43:41 +0000

    added MWE argparser for easier debugging

--- word2gauss/main.py
+++ word2gauss/main.py
@@ -52,11 +52,7 @@ padding = 0
 eta = 0.1 # learning rate : pass float for global learning rate (no min) or dict with keys mu,sigma,mu_min,sigma_min (local learning rate for each)
 Closs = 0.1 # regularization parameter in max-margin loss
 
-if args.MWE == 1:
-    report_schedule = 1000
-else:
-    report_schedule = 1000000
-    max_list_length = 6203
+
 
 ###############################################################################
 
@@ -110,192 +106,203 @@ def parse_args():
     return args
 
 
-args = parse_args()
+    args = parse_args()
 
-######################### LOAD DATA ###########################################
-if args.MWE == 1:
-    filename = 'war_and_peace.txt'
-    with open(filename, 'r') as file:
-        data = tokenizer(file.read().replace('\n', ' '))
-else:
-    print("\n\n----------- LOADING DATA ----------")
-    if os.path.exists("data_list.pkl"):
-    #     start = time.time()
-    #     print("loading from existing pickle")
-    #     pickle_in = open("data_list.pkl","rb")
-    #     data_list = pkl.load(pickle_in)
-    #     end = time.time()
-    #     print("loaded in {} secs".format(round(end - start,2)))
-    # else:
-        print("loading from gzip files")
-        files = []
-        for _, _, fs in os.walk("data/", topdown=False):
-            if args.MWE == 2:
-                files += [f for f in fs if f.endswith("00000.gz")]
-            else:
-                files += [f for f in fs if f.endswith(".gz")]
-
-        files = [os.path.join("data/page_dist_training_data/", f) for f in files]
-        data_list = []
-        for i, file in tqdm(enumerate(files)):
-            if padding == 0:
-                sentences = list(_open_file(file))
-                data_list += sentences
-            else:
-                sentences
-        # pickle_out = open("data_list.pkl","wb")
-        # pkl.dump(data_list, pickle_out)
-        # pickle_out.close()
-
-    if args.MWE == 2:
-        data_list = data_list[:2]
-
-    print("LOADING TO STRING")
-    lst = []
-    # data_list = data_list[:10]
-    for item in tqdm(data_list):
-      # print(item)
-      lst.append(listToString(item))
-    corpus = listToString(lst)
-    data = tokenizer(corpus)
-
-#print(corpus)
-#print(data)
-
-
-################################################################################
-
-
-
-
-
-
-
-
-
-entity_2_idx = defaultdict(lambda: len(entity_2_idx))
-counter = Counter()
-dataset = []
-
-for entity in tqdm(data):
-    entity_2_idx[entity]
-    counter[entity_2_idx[entity]] += 1
-    dataset.append(entity_2_idx[entity])
-
-# print(entity_2_idx)
-num_tokens = len(entity_2_idx)
-print("num_tokens = {}".format(num_tokens))
-
-
-#print(entity_2_idx)
-#print("\n\n")
-#print(counter)
-#print("\n\n")
-#print(dataset)
-dataset_length = len(dataset)
-print("Dataset length = {}".format(dataset_length))
-
-#### OLD ####
-
-# load the vocabulary
-vocab = Vocabulary(entity_2_idx,tokenizer)
-
-
-
-# create the embedding to train
-# use 100 dimensional spherical Gaussian with KL-divergence as energy function
-
-# embed = GaussianEmbedding(num_tokens, dimension,
-#     covariance_type=cov_type, energy_type=E_type)
-
-embed = GaussianEmbedding(N=num_tokens, size=dimension,
-          covariance_type=cov_type, energy_type=E_type,
-          mu_max=mu_max, sigma_min=sigma_min, sigma_max=sigma_max,
-          init_params={'mu0': mu0,
-              'sigma_mean0': sigma_mean0,
-              'sigma_std0': sigma_std0},
-          eta=eta, Closs=Closs,
-          iteration_verbose_flag=iteration_verbose_flag)
-
-
-# open the corpus and train with 8 threads
-# the corpus is just an iterator of documents, here a new line separated
-# gzip file for example
-
-
-print("---------- INITIAL EMBEDDING MEANS ----------")
-print(embed.mu)
-print("---------- INITIAL EMBEDDING COVS ----------")
-print(embed.sigma)
-
-
-
-epoch_losses = []
-for e in range(num_epochs):
-    print("---------- EPOCH {} ----------".format(e+1))
+    # set report schedule based on MWE case
     if args.MWE == 1:
-        with open(filename, 'r') as corpus:
-            print(corpus)
-            epoch_losses.append(embed.train(iter_pairs(corpus, vocab,batch_size=batch_size, nsamples=neg_samples, window=window), n_workers=num_workers, report_interval=report_schedule))
+        report_schedule = 1000
     else:
-        print(corpus)
-        print(dataset)
-        epoch_losses.append(embed.train(iter_pairs(dataset, vocab,batch_size=batch_size, nsamples=neg_samples, window=window), n_workers=num_workers, report_interval=report_schedule))
+        report_schedule = 1000000
+        max_list_length = 6203
 
-print("EPOCH LOSSES : {}".format(epoch_losses))
+    
+    ######################### LOAD DATA ###########################################
+    if args.MWE == 1:
+        filename = 'war_and_peace.txt'
+        with open(filename, 'r') as file:
+            data = tokenizer(file.read().replace('\n', ' '))
+    else:
+        print("\n\n----------- LOADING DATA ----------")
+        if os.path.exists("data_list.pkl"):
+        #     start = time.time()
+        #     print("loading from existing pickle")
+        #     pickle_in = open("data_list.pkl","rb")
+        #     data_list = pkl.load(pickle_in)
+        #     end = time.time()
+        #     print("loaded in {} secs".format(round(end - start,2)))
+        # else:
+            print("loading from gzip files")
+            files = []
+            for _, _, fs in os.walk("data/", topdown=False):
+                if args.MWE == 2:
+                    files += [f for f in fs if f.endswith("00000.gz")]
+                else:
+                    files += [f for f in fs if f.endswith(".gz")]
+
+            files = [os.path.join("data/page_dist_training_data/", f) for f in files]
+            data_list = []
+            for i, file in tqdm(enumerate(files)):
+                if padding == 0:
+                    sentences = list(_open_file(file))
+                    data_list += sentences
+                else:
+                    sentences
+            # pickle_out = open("data_list.pkl","wb")
+            # pkl.dump(data_list, pickle_out)
+            # pickle_out.close()
+
+        if args.MWE == 2:
+            data_list = data_list[:2]
+
+        print("LOADING TO STRING")
+        lst = []
+        # data_list = data_list[:10]
+        for item in tqdm(data_list):
+          # print(item)
+          lst.append(listToString(item))
+        corpus = listToString(lst)
+        data = tokenizer(corpus)
+
+    #print(corpus)
+    #print(data)
+
+
+    ################################################################################
 
 
 
 
-print("---------- FINAL EMBEDDING MEANS ----------")
-print(embed.mu)
-print("---------- FINAL EMBEDDING COVS ----------")
-print(embed.sigma)
-
-sigma_norms = np.linalg.norm(embed.sigma, axis=1)
-most_general_indices = np.split(sigma_norms,2)[0].argsort()[-10:][::-1]
-most_specific_indices = np.split(sigma_norms,2)[0].argsort()[:10]
-
-idx_2_entity = {v: k for k, v in entity_2_idx.items()}
-
-print("MOST GENERAL ENTITIES")
-for idx in most_general_indices:
-    print(idx_2_entity[idx])
-
-print("MOST SPECIFIC ENTITIES")
-for idx in most_specific_indices:
-    print(idx_2_entity[idx])
-
-# save the model for later
-if save_on:
-    print("SAVING MODEL")
-    embed.save('model_file_location_{}'.format(dimension), vocab=vocab.id2word, full=True)
-
-# print("LOADING MODEL")
-# test = GaussianEmbedding(N=num_tokens, size=dimension,
-#           covariance_type=cov_type, energy_type=E_type,
-#           mu_max=mu_max, sigma_min=sigma_min, sigma_max=sigma_max,
-#           init_params={'mu0': mu0,
-#               'sigma_mean0': sigma_mean0,
-#               'sigma_std0': sigma_std0},
-#           eta=eta, Closs=Closs)
-#
-# test.load('model_file_location')
 
 
-print("FINDING NEAREST NEIGHBOURS")
 
-target = "war"
-metric = cosine
-num = 10
 
-target_idx = entity_2_idx[target]
-neighbours = embed.nearest_neighbors(target=target_idx, metric=metric, num=num+1, vocab=vocab,
-                  sort_order='similarity')
 
-print("\n\n")
-print("Target = {}".format(target))
-print("Similarity metric = {}".format(metric))
-for i in range(1,num+1):
-    print("{}: {}".format(i,neighbours[i]))
-    # print("rank {}: word = {}, sigma = {}, id = {}, similarity = {}".format(i,neighbours[i][word],neighbours[i][sigma],neighbours[i][id],neighbours[i][similarity]))
+    entity_2_idx = defaultdict(lambda: len(entity_2_idx))
+    counter = Counter()
+    dataset = []
+
+    for entity in tqdm(data):
+        entity_2_idx[entity]
+        counter[entity_2_idx[entity]] += 1
+        dataset.append(entity_2_idx[entity])
+
+    # print(entity_2_idx)
+    num_tokens = len(entity_2_idx)
+    print("num_tokens = {}".format(num_tokens))
+
+
+    #print(entity_2_idx)
+    #print("\n\n")
+    #print(counter)
+    #print("\n\n")
+    #print(dataset)
+    dataset_length = len(dataset)
+    print("Dataset length = {}".format(dataset_length))
+
+    #### OLD ####
+
+    # load the vocabulary
+    vocab = Vocabulary(entity_2_idx,tokenizer)
+
+
+
+    # create the embedding to train
+    # use 100 dimensional spherical Gaussian with KL-divergence as energy function
+
+    # embed = GaussianEmbedding(num_tokens, dimension,
+    #     covariance_type=cov_type, energy_type=E_type)
+
+    embed = GaussianEmbedding(N=num_tokens, size=dimension,
+              covariance_type=cov_type, energy_type=E_type,
+              mu_max=mu_max, sigma_min=sigma_min, sigma_max=sigma_max,
+              init_params={'mu0': mu0,
+                  'sigma_mean0': sigma_mean0,
+                  'sigma_std0': sigma_std0},
+              eta=eta, Closs=Closs,
+              iteration_verbose_flag=iteration_verbose_flag)
+
+
+    # open the corpus and train with 8 threads
+    # the corpus is just an iterator of documents, here a new line separated
+    # gzip file for example
+
+
+    print("---------- INITIAL EMBEDDING MEANS ----------")
+    print(embed.mu)
+    print("---------- INITIAL EMBEDDING COVS ----------")
+    print(embed.sigma)
+
+
+
+    epoch_losses = []
+    for e in range(num_epochs):
+        print("---------- EPOCH {} ----------".format(e+1))
+        if args.MWE == 1:
+            with open(filename, 'r') as corpus:
+                print(corpus)
+                epoch_losses.append(embed.train(iter_pairs(corpus, vocab,batch_size=batch_size, nsamples=neg_samples, window=window), n_workers=num_workers, report_interval=report_schedule))
+        else:
+            print(corpus)
+            print(dataset)
+            epoch_losses.append(embed.train(iter_pairs(dataset, vocab,batch_size=batch_size, nsamples=neg_samples, window=window), n_workers=num_workers, report_interval=report_schedule))
+
+    print("EPOCH LOSSES : {}".format(epoch_losses))
+
+
+
+
+    print("---------- FINAL EMBEDDING MEANS ----------")
+    print(embed.mu)
+    print("---------- FINAL EMBEDDING COVS ----------")
+    print(embed.sigma)
+
+    sigma_norms = np.linalg.norm(embed.sigma, axis=1)
+    most_general_indices = np.split(sigma_norms,2)[0].argsort()[-10:][::-1]
+    most_specific_indices = np.split(sigma_norms,2)[0].argsort()[:10]
+
+    idx_2_entity = {v: k for k, v in entity_2_idx.items()}
+
+    print("MOST GENERAL ENTITIES")
+    for idx in most_general_indices:
+        print(idx_2_entity[idx])
+
+    print("MOST SPECIFIC ENTITIES")
+    for idx in most_specific_indices:
+        print(idx_2_entity[idx])
+
+    # save the model for later
+    if save_on:
+        print("SAVING MODEL")
+        embed.save('model_file_location_{}'.format(dimension), vocab=vocab.id2word, full=True)
+
+    # print("LOADING MODEL")
+    # test = GaussianEmbedding(N=num_tokens, size=dimension,
+    #           covariance_type=cov_type, energy_type=E_type,
+    #           mu_max=mu_max, sigma_min=sigma_min, sigma_max=sigma_max,
+    #           init_params={'mu0': mu0,
+    #               'sigma_mean0': sigma_mean0,
+    #               'sigma_std0': sigma_std0},
+    #           eta=eta, Closs=Closs)
+    #
+    # test.load('model_file_location')
+
+
+    print("FINDING NEAREST NEIGHBOURS")
+
+    target = "war"
+    metric = cosine
+    num = 10
+
+    target_idx = entity_2_idx[target]
+    neighbours = embed.nearest_neighbors(target=target_idx, metric=metric, num=num+1, vocab=vocab,
+                      sort_order='similarity')
+
+    print("\n\n")
+    print("Target = {}".format(target))
+    print("Similarity metric = {}".format(metric))
+    for i in range(1,num+1):
+        print("{}: {}".format(i,neighbours[i]))
+        # print("rank {}: word = {}, sigma = {}, id = {}, similarity = {}".format(i,neighbours[i][word],neighbours[i][sigma],neighbours[i][id],neighbours[i][similarity]))
+
+if __name__ == '__main__':
+    main_script()
