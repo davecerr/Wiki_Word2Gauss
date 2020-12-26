2a7d19d3c4bd322ed0a7733e682e37d9130edb4c
davide <davide@pop-os.localdomain>
2020-12-26 12:05:19 +0000

    wip changes

--- word2gauss/main.py
+++ word2gauss/main.py
@@ -179,9 +179,8 @@ print("Dataset length = {}".format(dataset_length))
 
 # load the vocabulary
 vocab = Vocabulary(entity_2_idx,tokenizer)
-print(vocab)
+print("PAIRINGS")
 pairings = iter_pairs(corpus, vocab,batch_size=batch_size, nsamples=neg_samples, window=window)
-print(pairings)
 
 
 # create the embedding to train
