cd52ca97aec0c6d48f558738c00efca5d606f755
davide <davide@pop-os.localdomain>
2020-12-26 11:46:20 +0000

    check start of array is being sampled correctly

--- word2gauss/main.py
+++ word2gauss/main.py
@@ -128,7 +128,8 @@ else:
         # pickle_out = open("data_list.pkl","wb")
         # pkl.dump(data_list, pickle_out)
         # pickle_out.close()
-
+    
+    data_list = data_list[:2]
     print("LOADING TO STRING")
     lst = []
     # data_list = data_list[:10]
