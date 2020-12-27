9a8209facde0cadf7ce625f1da713166d48c2b48
davide <28670484+davecerr@users.noreply.github.com>
2020-12-27 14:34:39 +0000

    print corpus length

--- word2gauss/main.py
+++ word2gauss/main.py
@@ -273,6 +273,7 @@ def main_script():
     epoch_losses = []
     for e in range(num_epochs):
         print("---------- EPOCH {} ----------".format(e+1))
+        print(len(corpus))
         if args.MWE == 1:
             with open('w_and_p.txt', 'r') as corpus:
                 epoch_losses.append(embed.train(iter_pairs(corpus, vocab, dynamic_window_size=args.dynamic_window_size, batch_size=batch_size, nsamples=neg_samples, window=window),
