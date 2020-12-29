69886da9b27557bdad22b35945b64c82a7d73554
davide <28670484+davecerr@users.noreply.github.com>
2020-12-29 11:07:55 +0000

    wip changes

--- word2gauss/scheduler.sh
+++ word2gauss/scheduler.sh
@@ -6,4 +6,4 @@
 #$ -j y
 #$ -N Test W2G Embedding
 # Commands to be executed go here...
-python main.py --MWE=2 --dynamic_window_size=True --report_schedule=1000 --num_epochs=2 --dim=5 --neg_samples=2 --eta=0.1 --Closs=0.1 --save=False --csv=True
+python main.py --MWE=2 --dynamic_window_size=True --report_schedule=1000 --num_epochs=2 --dim=5 --neg_samples=2 --eta=0.1 --Closs=0.1 --save=False --csv=True --num_threads=1
