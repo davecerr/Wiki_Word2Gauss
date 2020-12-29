e20bed4bc66879fea648816070fc3cba2cc37fcb
davide <28670484+davecerr@users.noreply.github.com>
2020-12-29 11:09:06 +0000

    wip changes

--- word2gauss/scheduler.sh
+++ word2gauss/scheduler.sh
@@ -1,9 +1,7 @@
-# This is an example of a job script.
 #$ -S /bin/bash
 #$ -l h_rt=0:10:0
 #$ -l h_vmem=2G,tmem=2G
 #$ -cwd
 #$ -j y
 #$ -N Test W2G Embedding
-# Commands to be executed go here...
 python main.py --MWE=2 --dynamic_window_size=True --report_schedule=1000 --num_epochs=2 --dim=5 --neg_samples=2 --eta=0.1 --Closs=0.1 --save=False --csv=True --num_threads=1
