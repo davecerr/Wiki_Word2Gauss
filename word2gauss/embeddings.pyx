7d7dc609a2a4c5ab485c7a5539c8082ffffcff44
davide <28670484+davecerr@users.noreply.github.com>
2021-01-01 11:59:31 +0000

    try adjusting thread processing

--- word2gauss/embeddings.pyx
+++ word2gauss/embeddings.pyx
@@ -772,7 +772,8 @@ cdef class GaussianEmbedding:
         # each job is a batch of pairs from the iterator
         # add jobs to a queue, workers pop from the queue
         # None means no more jobs
-        #jobs = PriorityQueue(maxsize=2 * n_workers)
+        '''
+        jobs = Queue(maxsize=2 * n_workers)
 
         # number processed, next time to log, logging interval
         # make it a list so we can modify it in the thread w/o a local var
@@ -811,27 +812,57 @@ cdef class GaussianEmbedding:
                         processed[1] = processed[0] + processed[2]
                         if reporter:
                             reporter(self, processed[0])
+        '''
+
+
+        pqueue = PriorityQueue()
+        def threading_work(c,pairs):
+            while True:
+                if pairs is None:
+                    # no more data
+                    break
+                if pairs.shape[0] == 0:
+                    LOGGER.info("TERMINATING. Pairs shape =")
+                    print pairs.shape
+                    break
+                if verbose_pairs:
+                    if c == 1:
+                        print(pairs.shape)
+                        for j in range(pairs.shape[0]):
+                            print pairs[j,:]
+                batch_loss = self.train_batch(pairs)
+                with lock:
+                    processed[0] += 1
+                    if processed[1] and processed[0] >= processed[1]:
+                        t2 = time.time()
+                        self.epoch_loss += batch_loss
+                        LOGGER.info(">>>>>>>>>> Batch %s, Batch Loss %f, Epoch Loss %f, elapsed time: %s <<<<<<<<<<"
+                                    % (processed[0], batch_loss, self.epoch_loss, t2 - t1))
+                        processed[1] = processed[0] + processed[2]
+                        if reporter:
+                            reporter(self, processed[0])
+
 
         # start threads
-        threads = []
-        for k in range(n_workers):
-            thread = Thread(target=_worker)
-            thread.daemon = True
+        threadsPool = []
+        data = []
+
+        for c, batch_pairs in enumerate(iter_pairs):
+            t = threading.Thread(target=threading_work, args=(c, batch_pairs))
+            threadsPool.append(t)
+
+        print(len(threadsPool))
+        for thread in threadsPool:
             thread.start()
-            threads.append(thread)
 
-        # put data on the queue!
-        for batch_pairs in iter_pairs:
-            jobs.put(batch_pairs)
-
-        # no more data, tell the threads to stop
-        for i in range(len(threads)):
-            jobs.put(None)
-
-        # now join the threads
-        for thread in threads:
+        for thread in threadsPool:
             thread.join()
 
+        while not pqueue.empty()
+            data.append(pqueue.get())
+
+        print(len(data))
+        
         LOGGER.info("\n\nEpoch Loss %f" % self.epoch_loss)
         return self.epoch_loss
 
