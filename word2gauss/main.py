a70fd9b47de94f4426d72fd976002d624b7ee3fa
davide <28670484+davecerr@users.noreply.github.com>
2021-01-01 15:54:50 +0000

    run subset of data in MWE=2 for debugging of pair ordering

--- word2gauss/main.py
+++ word2gauss/main.py
@@ -235,8 +235,8 @@ def main_script():
             # pkl.dump(data_list, pickle_out)
             # pickle_out.close()
 
-        #if args.MWE == 2:
-            #data_list = data_list[:2]
+        if args.MWE == 2:
+            data_list = data_list[:2]
 
 
         print("WRITING DATA")
