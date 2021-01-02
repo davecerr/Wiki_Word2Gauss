ebab2fcf41f09e68d7f02e5a6a279253466d7bf5
davide <28670484+davecerr@users.noreply.github.com>
2021-01-02 12:08:29 +0000

    correct calculation of batch loss

--- word2gauss/embeddings.pyx
+++ word2gauss/embeddings.pyx
@@ -878,7 +878,9 @@ cdef class GaussianEmbedding:
                     break
 
                 #with lock:
-                batch_loss = self.train_batch(k, pairs)
+                loss = self.train_batch(k, pairs)
+                batch_loss += loss
+                self.epoch_loss += loss
 
                 if verbose_pairs:
                     for j in range(pairs.shape[0]):
@@ -890,7 +892,6 @@ cdef class GaussianEmbedding:
                     processed[0] += 1
                     if processed[1] and processed[0] >= processed[1]:
                         t2 = time.time()
-                        self.epoch_loss += batch_loss
                         LOGGER.info(">>>>>>>>>> Batch %s/%s, Batch Loss %f, Epoch Loss %f, elapsed time: %s <<<<<<<<<<"
                                     % (processed[0], total_num_examples, batch_loss, self.epoch_loss, t2 - t1))
                         processed[1] = processed[0] + processed[2]
