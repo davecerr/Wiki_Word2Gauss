a6eed6a10fd086956a67b4b14fb58fb7affc26d8
davide <28670484+davecerr@users.noreply.github.com>
2021-01-09 12:14:07 +0000

    test loading of gzipped WiRe data

--- word2gauss/main.py
+++ word2gauss/main.py
@@ -329,6 +329,7 @@ def main_script():
             for i, file in tqdm(enumerate(files)):
                     sentences = list(_open_file(file))
                     data_list += sentences
+            print(data_list[:1000])
             # pickle_out = open("data_list.pkl","wb")
             # pkl.dump(data_list, pickle_out)
             # pickle_out.close()
