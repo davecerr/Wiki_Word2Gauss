5e1c308e6500343f1bd137d8d4663d580b855a69
davide <28670484+davecerr@users.noreply.github.com>
2021-01-09 12:47:51 +0000

    test loading of gzipped WiRe data

--- word2gauss/main.py
+++ word2gauss/main.py
@@ -51,7 +51,7 @@ batch_size=10
 validation_path = "data/WiRe.csv"
 
 # calculation/printing controls
-print_init_embeddings = True
+print_init_embeddings = False
 print_final_embeddings = False
 calc_general_and_specific = False
 calc_similarity_example = True
