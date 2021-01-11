d4ca3b7130f969cf85714762a6876d3c4bcb10b9
davide <28670484+davecerr@users.noreply.github.com>
2021-01-11 15:48:24 +0000

    file to build adjacency matrix

--- word2gauss/adjacency_matrix.py
+++ word2gauss/adjacency_matrix.py
@@ -28,6 +28,7 @@ with gzip.open(files[0], 'r') as fin:        # 4. gzip
     json_bytes = fin.read()                      # 3. bytes (i.e. UTF-8)
 
 json_str = json_bytes.decode('utf-8')            # 2. string (i.e. JSON)
+print(json_str)
 data = json.loads(json_str)                      # 1. data
 
 print(data)
